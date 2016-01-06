e3ca244ce7e05d9ab29511deee1492dddb1695bc
bitrix <bitrix@localhost.localdomain>
2016-01-06 17:18:16 +0300

    Main site template have been created

---
+++ local/templates/main/description.php
@@ -0,0 +1,8 @@
+<?php?>
+/**
+ * Created by PhpStorm.
+ * User: Георгий
+ * Date: 06.01.2016
+ * Time: 15:45
+ */
+<? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die(); ?> <? IncludeTemplateLangFile(__FILE__); $arTemplate = Array(     "NAME" => GetMessage("TEMPLATE_DESCRIPTION_NAME"),     "DESCRIPTION" => GetMessage("TEMPLATE_DESCRIPTION_DESC") ); ?>
---
+++ local/templates/main/include_areas/logo.php
@@ -0,0 +1,8 @@
+<?php?>
+/**
+ * Created by PhpStorm.
+ * User: Георгий
+ * Date: 05.01.2016
+ * Time: 21:02
+ */
+<img src="<?=SITE_TEMPLATE_PATH;?>/images/index_07.jpg" width="733" height="319" alt="logo">
